acd1377eef8a4089ce405f82eb1c150cbf8e4585
Ali Rezaei <im1.bitcode@gmail.com>
2024-02-03 15:47:26 +0330

    add particles

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
       <body className={inter.className}>
         <ActiveSectionContextProvider>
-          {/* <ParticlesPattern /> */}
+          <ParticlesPattern />
           <Header />
           <Navbar />
           {children}
