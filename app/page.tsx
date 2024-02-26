806ee4ff3b9dc92837e48207fe3a870dfa0c8d23
Ali Rezaei <im1.bitcode@gmail.com>
2024-02-26 12:11:46 +0330

    errors in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import About from "@/sections/About";
+import Contact from "@/sections/Contact";
+import Experience from "@/sections/Experience";
+import Intro from "@/sections/Intro";
+import Projects from "@/sections/Projects";
+import Skills from "@/sections/Skills";
 import SectionDivider from "@/components/SectionDivider";
-import About from "@/components/sections/About";
-import Contact from "@/components/sections/Contact";
-import Experience from "@/components/sections/Experience";
-import Intro from "@/components/sections/Intro";
-import Projects from "@/components/sections/Projects";
-import Skills from "@/components/sections/Skills";
 
 
 export default function Home() {
